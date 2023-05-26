e4f17c83eca0a295a7476376a2bb1d673eab56ce
abyan15 <altorianyazid@gmail.com>
2023-05-26 18:02:27 +0700

    Initial commit

---
+++ app/src/main/java/com/abyan/utspraktik_if6_10120209_abyanaltoriandiyazid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.abyan.utspraktik_if6_10120209_abyanaltoriandiyazid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
